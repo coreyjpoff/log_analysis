a1601f3bb452e7fb1394f41acd2421b831b27d0d
coreyjpoff <coreypoff@gmail.com>
2017-09-28 13:00:10 -0400

    Feat: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# log_analysis
+Command line application that interfaces with the news
+database to give a report on a few FAQs
+
+To run, enter command:
+ `python log_analysis.py`
+
+The results will be printed to the console. No views are used.
+
+**_What are the most popular three articles of all time?_**
+Method:
+ - count the log table's views grouped by path
+ - where the path starts with 'article'
+ - join on the path col with the article table's slug col
+ - order by the most views and limited to 3
+
+**_What are the most popular article authors of all time?_**
+Method:
+ - count the log table's views grouped by path
+ - where the path starts with 'article'
+ - join with the articles table on the slug
+ - join with the authors table on the id
+ - group by the author name
+ - and order with the most views at the top
+
+**_On which days did more than 1% of requests lead to errors?_**
+Method:
+ - Truncate the time column to be just date, and count the requests grouped by day in the log table
+ - join with another log query, this one also truncating the time column to the date, and only counting queries that resulted in an error in the status column; these are also grouped by day
+ - They are joined on the date (truncated time columns)
+ - And filtered to only days where the portion of total requests that resulted in error is greater than 0.01.
---
+++ log_analysis.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+from psycopg2 import connect
+from decimal import Decimal
+
+DBNAME = "news"
+
+
+def findMostReadArticles():
+    db = connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select articles.title, articleCounts.views " +
+        "from (select path, count(*) as views " +
+        "from log group by path " +
+        "having substring(path from 1 for 9)='/article/') articleCounts " +
+        "join articles " +
+        "on articleCounts.path = '/article/' || articles.slug " +
+        "order by articleCounts.views desc " +
+        "limit 3;"
+    )
+    articles = c.fetchall()
+    db.close()
+    return articles
+
+
+def findMostReadAuthors():
+    db = connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select auth.name, sum(c.views) as totalViews " +
+        "from (select path, count(*) as views " +
+        "from log group by path " +
+        "having substring(path from 1 for 9)='/article/') c " +
+        "join articles art " +
+        "on c.path = '/article/' || art.slug " +
+        "join authors auth " +
+        "on art.author = auth.id " +
+        "group by auth.name " +
+        "order by totalViews desc; "
+    )
+    authors = c.fetchall()
+    db.close()
+    return authors
+
+
+def printMostRead(viewsTable):
+    for row in viewsTable:
+        print row[0] + " -- " + str(row[1]) + " views"
+
+
+def findDaysWithErrors(percent):
+    db = connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select totalRequests.date, " +
+        "(errors.errCount*100.0 / totalRequests.totalCount) " +
+        "from (select date_trunc('day', time) as date, " +
+        "count(*) as totalCount " +
+        "from log " +
+        "group by date_trunc('day', time)) totalRequests " +
+        "join (select date_trunc('day', time) as date, " +
+        "count(*) as errCount from log " +
+        "where status = '404 NOT FOUND' " +
+        "group by date_trunc('day', time) " +
+        ") errors " +
+        "on totalRequests.date=errors.date " +
+        "where (errors.errCount*100.0 / totalRequests.totalCount) >= " +
+        str(percent)
+    )
+    errDays = c.fetchall()
+    db.close()
+    return errDays
+
+
+def printDaysWithErrors(errDays):
+    for errDay in errDays:
+        print errDay[0].strftime('%B %d, %Y') +
+        " -- " +
+        str(round(errDay[1], 1)) + "% errors"
+
+
+if __name__ == '__main__':
+    print '\n\n\nWhat are the most popular three articles of all time?\n'
+    articles = findMostReadArticles()
+    printMostRead(articles)
+    print '\n\n\nWho are the most popular article authors of all time?\n'
+    authors = findMostReadAuthors()
+    printMostRead(authors)
+    print '\n\n\nOn which days did more than 1% of requests lead to errors?\n'
+    errDays = findDaysWithErrors(1)
+    printDaysWithErrors(errDays)
+    print '\n'
